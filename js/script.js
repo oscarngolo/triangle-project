db38dfb4ddccec20b2b46f1ce0d507b2cd5c606c
oscar ngolo <oscarngoloman@yahoo.com>
2018-11-30 10:57:43 +0300

    add  javascript file to my web page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
 var triangleTracker = function(){
-  var side1 = parseInt(document.getElementById("sideA").value);
-  var side2 = parseInt(document.getElementByid("sideB").value);
-  var side3 = parseInt(document.getElementById("sideC").value);
+  var side1 = parseFloat(document.getElementById("sideA").value)
+  var side2 = parseFloat(document.getElementById("sideB").value)
+  var side3 = parseFloat(document.getElementById("sideC").value)
 
-  var output= document.getElementByname(output);
-
-  if (side1 === side2 || side1 === side3 || side2 === side3) {
-    output.textContent = "isosceles triangle";
-  } else if (side1 === side2 && side1 === side3 && side2 === side3) {
-    output.textContent = "equilateral triangle";
-  } else if (side1 !== side2 || side1 !== side3 || side2 !== side3) {
-    output.textContent = "scalene triangle";
-  } else {
-    output.textContent = "NOT a triangle";
-  }
+  if (side1 === side2 && side1 === side3 && side2 === side3) {
+    alert("equilateral triangle");
+  } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+    alert("isosceles triangle");
+  } else if(side1 !== side2 || side2 !== side3 || side1 !== side3){
+    alert("scalene triangle");
+  } else ()
 }
