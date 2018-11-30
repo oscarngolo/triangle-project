59e2a3de0174ba2c74ae41989982b89a1640984b
oscar ngolo <oscarngoloman@yahoo.com>
2018-11-30 16:09:16 +0300

    add changes on my readme in my web page

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ var triangleTracker = function(){
   var side2 = parseInt(document.getElementById("sideB").value);
   var side3 = parseInt(document.getElementById("sideC").value);
 
-  if (side1 === side2 && side1 === side3 && side2 === side3) {
+  if (side1 == side2 && side1 == side3 && side2 == side3) {
     prompt("The type of a triangle is?","equilateral triangle");
-  } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+  } else if (side1 == side2 || side1 == side3 || side2 == side3) {
     prompt("The type of a triangle is?", "isosceles triangle");
-  } else if(side1 !== side2 || side2 !== side3 || side1 !== side3){
+  } else if((side1 + side2) <= (side3) || (side1 + side3) <= (side2) || (side3 + side2) <= (side1)){
     prompt("The type of a triangle is?", "scalene triangle");
   } else {
     prompt("The type of a triangle is?", "NOT a triangle");
