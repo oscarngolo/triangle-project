6e824e93d6e2593d211baf2ad2baf37985d7c87b
oscar ngolo <oscarngoloman@yahoo.com>
2018-12-02 10:54:23 +0300

    add changes in my web page

--- js/script.js
+++ js/script.js
@@ -4,12 +4,26 @@ var triangleTracker = function(){
   var side3 = parseInt(document.getElementById("sideC").value);
 
   if (side1 == side2 && side1 == side3 && side2 == side3) {
+
     prompt("The type of a triangle is?","equilateral triangle.");
-  } else if (side1 == side2 || side1 == side3 || side2 == side3) {
+
+  }
+
+  else if (side1 == side2 || side1 == side3 || side2 == side3) {
+
     prompt("The type of a triangle is?", "isosceles triangle.");
-  } else if((side1 + side2) <= (side3) || (side1 + side3) <= (side2) || (side3 + side2) <= (side1)){
+
+  }
+
+  else if((side1 + side2) <= (side3) || (side1 + side3) <= (side2) || (side3 + side2) <= (side1)){
+
     prompt("The type of a triangle is?", "scalene triangle.");
-  } else {
+
+  }
+
+  else {
+
     prompt("The type of a triangle is?", "NOT a triangle.");
+
   }
 }
