1b1c231cc0e49edb064f52c95b3631ad8fd31590
oscar ngolo <oscarngoloman@yahoo.com>
2018-12-03 09:53:02 +0300

    add all changes made on html and js

--- js/script.js
+++ js/script.js
@@ -3,27 +3,29 @@ var triangleTracker = function(){
   var side2 = parseInt(document.getElementById("sideB").value);
   var side3 = parseInt(document.getElementById("sideC").value);
 
-  if (side1 == side2 && side1 == side3 && side2 == side3) {
-
-    prompt("The type of a triangle is?","equilateral triangle.");
+  var result;
+    if (side1 == side2 && side1 == side3 && side2 == side3) {
+      result = "equilateral triangle."
+      console.log(result);
+      document.getElementById("output").innerHTML = result;
 
   }
 
   else if (side1 == side2 || side1 == side3 || side2 == side3) {
-
-    prompt("The type of a triangle is?", "isosceles triangle.");
-
+    result = "isosceles triangle."
+    console.log(result);
+  document.getElementById("output").innerHTML = result;
   }
 
   else if((side1 + side2) <= (side3) || (side1 + side3) <= (side2) || (side3 + side2) <= (side1)){
-
-    prompt("The type of a triangle is?", "scalene triangle.");
-
+    result = "scalene triangle."
+    console.log(result);
+  document.getElementById("output").innerHTML = result;
   }
 
   else {
-
-    prompt("The type of a triangle is?", "NOT a triangle.");
-
-  }
+    result = "This is NOT a triangle."
+    console.log(result);
+  document.getElementById("output").innerHTML = result;
+      }
 }
