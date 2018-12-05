c440e046e28f9d9f0b290fe16289ea91418fe8bd
oscar ngolo <oscarngoloman@yahoo.com>
2018-12-05 14:38:10 +0300

    add all changes made

--- README.md
+++ README.md
@@ -5,16 +5,13 @@
 Triangle tracker is used to identify  the type of triangle whenever we key in a value in the input boxes as shown on the page. It has listed the types of triangles to help us figure out the type as we key in.
 ![](images/img.jpg);
 
-
 ## Setup Instructions
-
 
 All you need is to insert your figures on the input box then click on the type of triangle button right below the input boxes and examine what happens.
 
 ## BDD
 
-When you visit my site, all you need to do is to input your values in the spaces provided, when you click on the type of triangle button, you will see your results below the button indicating the results of your input. for example when you insert values like 4,4,4 the results will be equilateral triangle and it will be indicated below the type of triangle button.
-
+![](images/bdd.jpg);
 
 ## Author Information
 
--- js/script.js
+++ js/script.js
@@ -1,31 +1,28 @@
-var triangleTracker = function(){
+var triangleTracker = function() {
   var side1 = parseInt(document.getElementById("sideA").value);
   var side2 = parseInt(document.getElementById("sideB").value);
   var side3 = parseInt(document.getElementById("sideC").value);
 
   var result;
-    if (side1 == side2 && side1 == side3 && side2 == side3) {
-      result = "equilateral triangle."
-      console.log(result);
-      document.getElementById("output").innerHTML = result;
+  
+  if (side1 == side2 && side1 == side3 && side2 == side3) {
+    result = "equilateral triangle."
+    console.log(result);
+    document.getElementById("output").innerHTML = result;
 
-  }
-
-  else if (side1 == side2 || side1 == side3 || side2 == side3) {
+  } else if (side1 == side2 || side1 == side3 || side2 == side3) {
     result = "isosceles triangle."
     console.log(result);
-  document.getElementById("output").innerHTML = result;
-  }
+    document.getElementById("output").innerHTML = result;
 
-  else if((side1 + side2) <= (side3) || (side1 + side3) <= (side2) || (side3 + side2) <= (side1)){
+  } else if ((side1 + side2) <= (side3) || (side1 + side3) <= (side2) || (side3 + side2) <= (side1)) {
     result = "This is NOT a triangle."
     console.log(result);
-  document.getElementById("output").innerHTML = result;
-  }
+    document.getElementById("output").innerHTML = result;
 
-  else {
+  } else {
     result = "scalene triangle."
     console.log(result);
-  document.getElementById("output").innerHTML = result;
-      }
+    document.getElementById("output").innerHTML = result;
+  }
 }
